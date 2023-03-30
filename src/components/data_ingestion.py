e6038d5898c644e5a988c6e898c1f8f4157cdab9
vucina1906 <vucina19931906@gmail.com>
2023-03-30 10:55:53 +0200

    model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,17 +3,21 @@ import sys
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
+
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 @dataclass
 class DataIngestionConfig:
-    train_data_path:str=os.path.join('artifacts',"train.csv")
-    test_data_path:str=os.path.join('artifacts',"test.csv")
-    raw_data_path:str=os.path.join('artifacts',"raw.csv")
+    train_data_path: str=os.path.join('artifacts',"train.csv")
+    test_data_path: str=os.path.join('artifacts',"test.csv")
+    raw_data_path: str=os.path.join('artifacts',"data.csv")
 
-#As soon as I call class DataIngestion than all train_data_path,test_data_path and raw_data_path
-#will be saved in ingestion_config variable as a subobjects
 class DataIngestion:
     def __init__(self):
         self.ingestion_config=DataIngestionConfig()
@@ -44,10 +48,13 @@ class DataIngestion:
             )
         except Exception as e:
             raise CustomException(e,sys)
-
+        
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
-        
-    
-    
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
